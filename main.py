8a7f23a7e4635749c49fee52b08c9a0d6bf1c81a
daemian19 <hptrivedi1@sheffield.ac.uk>
2026-04-21 13:29:38 +0100

    Initial draft/changes in the code  with pdf report

--- main.py
+++ main.py
@@ -6,66 +6,75 @@ class CustomerManager:
         self.discount_threshold = 500
 
     def add_customer(self, name, purchases):
-        if name in self.customers.keys():
+        """Add a customer or extend their purchases."""
+        if name in self.customers:
             self.customers[name].extend(purchases)
         else:
             self.customers[name] = purchases
 
     def add_purchase(self, name, purchase):
+        """Add a single purchase for a customer."""
         self.add_customer(name, [purchase])
 
     def add_purchases(self, name, purchases):
+        """Add multiple purchases for a customer."""
         self.add_customer(name, purchases)
 
+    def _calculate_total_purchases(self, purchases):
+        """Calculate the total price of purchases, applying tax if necessary."""
+        total = 0
+        for purchase in purchases:
+            price = purchase['price']
+            if price > self.tax_threshold:
+                price *= (1 + self.tax_rate)
+            total += price
+        return total
+
+    def _determine_customer_status(self, total):
+        """Determine the status of a customer based on their total purchases."""
+        status = []
+        if total > self.discount_threshold:
+            status.append("Eligible for discount")
+        elif total > 300:
+            status.append("Potential future discount customer")
+        else:
+            status.append("No discount")
+
+        if total > 1000:
+            status.append("VIP Customer!")
+        elif total > 800:
+            status.append("Priority Customer")
+
+        return status
+
     def generate_report(self):
-        for y, x in self.customers.items():
-            a = 0
-            for z in x:
-                if z['price'] > self.tax_threshold:
-                    taxed_price = z['price'] * (1 + self.tax_rate)
-                    a += taxed_price
-                else:
-                    a += z['price']
-            print(y)
-            if a > self.discount_threshold:
-                print("Eligible for discount")
-            else:
-                if a > 300:
-                    print("Potential future discount customer")
-                else:
-                    print("No discount")
-            if a > 1000:
-                print("VIP Customer!")
-            else:
-                if a > 800:
-                    print("Priority Customer")
+        """Generate a report for all customers."""
+        for name, purchases in self.customers.items():
+            total = self._calculate_total_purchases(purchases)
+            print(name)
+            for status in self._determine_customer_status(total):
+                print(status)
+
+    @staticmethod
+    def _has_heavy_item(purchases):
+        """Check if any purchase is heavy."""
+        return any(purchase.get('weight', 0) > 20 for purchase in purchases)
+
+    @staticmethod
+    def _has_fragile_item(purchases):
+        """Check if any purchase is fragile."""
+        return any(purchase.get('fragile', False) for purchase in purchases)
 
     def calculate_shipping_fee(self, purchases):
-        heavy_item = False
-        for purchase in purchases:
-            if purchase.get('weight', 0) > 20:
-                heavy_item = True
-                break
-        if heavy_item:
-            return 50
-        else:
-            return 20
+        """Calculate shipping fee based on purchase weight."""
+        return 50 if self._has_heavy_item(purchases) else 20
 
 def calculate_shipping_fee_for_heavy_items(purchases):
-    for purchase in purchases:
-        if purchase.get('weight', 0) > 20:
-            return 50
-    return 20
+    """Calculate shipping fee for heavy items."""
+    return 50 if CustomerManager._has_heavy_item(purchases) else 20
 
 def calculate_shipping_fee_for_fragile_items(purchases):
-    fragile_item = False
-    for purchase in purchases:
-        if purchase.get('fragile', False):
-            fragile_item = True
-            break
-    if fragile_item:
-        return 60
-    else:
-        return 25
+    """Calculate shipping fee for fragile items."""
+    return 60 if CustomerManager._has_fragile_item(purchases) else 25
 
 flat_tax = 0.2
